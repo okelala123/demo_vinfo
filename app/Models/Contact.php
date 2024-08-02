69a1c04bebcce2411d5d08e8cc8389974cd4b9b1
okelala123 <daicmngia@gmail.com>
2024-08-02 14:32:33 +0700

    Thêm SoftDeletes trait vào Prices model và các thay đổi khác

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Providers;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Contact extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
     protected $table = 'contacts';
 
     // public function providers()
